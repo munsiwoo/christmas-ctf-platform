7f84e98e0811e27dfaeb99bfda91bb42bc8e3df6
Siwoo Mun <mun.xiwoo@gmail.com>
2021-10-27 11:11:59 +0900

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,17 +1,7 @@
 <?php
 error_reporting(0);
-# made by munsiwoo
-
-ini_set("session.cache_expire", 3600*24);
-ini_set("session.gc_maxlifetime", 3600*24); // 세션 만료기간 늘림
 session_name('session_id');
 session_start();
-
-$ban_browser = "/Edge\/18|Trident\/4\.0|Trident\/7\.0/";
-if(preg_match($ban_browser, $_SERVER['HTTP_USER_AGENT'])) {
-    echo '<meta http-equiv="refresh" content="3;url=https://www.google.com/chrome/">';
-    die('Your browser is not supported. (Ie, Edge)'); // Ie하고 구버전 Edge는 렌더 엔진이 좀 이상하다.
-}
 
 require_once $_SERVER['DOCUMENT_ROOT'].'/config/config.php';
 require_once $_SERVER['DOCUMENT_ROOT'].'/config/function.php';
